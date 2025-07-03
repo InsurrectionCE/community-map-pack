bcc83d88fec1a670b33b28173bac021a11348841
markmcfuzz <magecape10@gmail.com>
2025-07-03 12:24:41 -0600

    project setup

---
+++ lua/community_map_pack.lua
@@ -0,0 +1,110 @@
+local balltze = Balltze
+local engine = Engine
+DebugMode = true
+package.preload["luna"] = nil
+package.loaded["luna"] = nil
+require "luna"
+
+-- local skullsManager = require "alpha_halo.gameplay_core.skullsManager"
+local commands = require "alpha_halo.systems.firefight.commands"
+-- local firefightManager = require "alpha_halo.firefightManager"
+
+-- local main
+local loadWhenIn = {"alpha_halo"}
+
+loadWhenIn = table.extend(loadWhenIn, table.map(loadWhenIn, function(map)
+    return map .. "_dev"
+end))
+
+function PluginMetadata()
+    return {
+        name = "Alpha Halo",
+        author = "El Dream Team",
+        version = "1.0.0",
+        targetApi = "1.0.0",
+        reloadable = true,
+        maps = loadWhenIn
+    }
+end
+
+local function loadChimeraCompatibility()
+    -- Load Chimera compatibility
+    for k, v in pairs(balltze.chimera) do
+        if not k:includes "timer" and not k:includes "execute_script" and
+            not k:includes "set_callback" then
+            _G[k] = v
+        end
+    end
+    server_type = engine.netgame.getServerType()
+
+    -- Replace Chimera functions with Balltze functions
+    write_bit = balltze.memory.writeBit
+    write_byte = balltze.memory.writeInt8
+    write_word = balltze.memory.writeInt16
+    write_dword = balltze.memory.writeInt32
+    write_int = balltze.memory.writeInt32
+    write_float = balltze.memory.writeFloat
+    write_string = function(address, value)
+        for i = 1, #value do
+            write_byte(address + i - 1, string.byte(value, i))
+        end
+        if #value == 0 then
+            write_byte(address, 0)
+        end
+    end
+    execute_script = engine.hsc.executeScript
+end
+
+local main
+
+function PluginFirstTick()
+    balltze.event.tick.subscribe(function(event)
+        if event.time == "before" then
+            if not main then
+                main = require "alpha_halo.main"
+            end
+        end
+    end)
+end
+
+function PluginLoad()
+    logger = balltze.logger.createLogger("Alpha Halo") -- this means Alpha Firefight
+    logger:muteDebug(not DebugMode)
+    logger:muteIngame(not DebugMode)
+    loadChimeraCompatibility()
+    Balltze.event.frame.subscribe(function(ev)
+        if ev.time == "before" then
+            local font = "smaller"
+            local align = "center"
+            local bounds = {left = 0, top = 400, right = 640, bottom = 480}
+            local textColor = {1.0, 0.45, 0.72, 1.0}
+            local memory = collectgarbage("count")
+            local sizeInMb = memory / 1024
+            local text = string.format("Lua Memory Usage: %.4f MB", sizeInMb)
+            Balltze.chimera.draw_text(text, bounds.left, bounds.top, bounds.right, bounds.bottom,
+                                      font, align, table.unpack(textColor))
+        end
+    end)
+
+    -- Commands for Alpha Firefight
+    for command, data in pairs(commands) do
+        -- local command = command:replace("debug_", "")
+        balltze.command.registerCommand(command, command, data.description, data.help,
+                                        data.save or false, data.minArgs or 0, data.maxArgs or 0,
+                                        false, true, function(...)
+            data.func(table.unpack(...))
+            return true
+        end)
+    end
+    balltze.command.loadSettings()
+
+    Balltze.command.registerCommand("bye", "misc", "Prints 'Hello World' to the console", nil, false, 0, 0, true, true, function()
+        lua_memory_snapshot()
+        return true
+    end)
+
+    return true
+end
+
+function PluginUnload()
+end
